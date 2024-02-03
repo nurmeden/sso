3112c4885012f378e8dd8f1ed07d5b5029d3ff7b
nurmeden <nurmeden.02@gmail.com>
2024-02-03 15:27:25 +0600

    feat: Логгирование

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "github.com/nurmeden/sso/internal/config"
+import (
+	"log/slog"
+	"os"
+
+	"github.com/nurmeden/sso/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev = "dev"
+	envProd = "prod"
+)
 
 func main() {
 	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+        log = slog.New(
+            slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+        )
+    case envProd:
+        log = slog.New(
+            slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+        )
+    }
+
+	return log
 }
