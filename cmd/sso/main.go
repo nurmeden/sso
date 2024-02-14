cd1af5d63f750d92a117ff9b9856c962ecf4fcd0
nurmeden <nurmeden.02@gmail.com>
2024-02-14 19:43:16 +0600

    refactor(sso): Запуск сервера и добавил Graceful shutdown

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,22 +1,40 @@
 package main
 
 import (
+	"github.com/nurmeden/sso/internal/app"
 	"log/slog"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/nurmeden/sso/internal/config"
 )
 
 const (
 	envLocal = "local"
-	envDev = "dev"
-	envProd = "prod"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
 	log := setupLogger(cfg.Env)
+
+	application := app.New(log, cfg.GRPC.Port, cfg.StoragePath, cfg.TokenTTL)
+
+	go func() {
+		application.GRPCServer.MustRun()
+	}()
+
+	stop := make(chan os.Signal, 1)
+
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+
+	<-stop
+
+	application.GRPCServer.Stop()
+	log.Info("Gracefully stopped")
 }
 
 func setupLogger(env string) *slog.Logger {
@@ -28,14 +46,14 @@ func setupLogger(env string) *slog.Logger {
 			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
 		)
 	case envDev:
-        log = slog.New(
-            slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
-        )
-    case envProd:
-        log = slog.New(
-            slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
-        )
-    }
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
 
 	return log
 }
