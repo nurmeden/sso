362d283dd848bc04b106ef48ea0409661aa78f70
nurmeden <nurmeden.02@gmail.com>
2024-02-14 19:31:02 +0600

    feat(App): Создание основного приложения

---
+++ internal/app/app.go
@@ -0,0 +1,28 @@
+package app
+
+import (
+	grpcapp "github.com/nurmeden/sso/internal/app/grpc"
+	"github.com/nurmeden/sso/internal/services/auth"
+	"github.com/nurmeden/sso/internal/storage/sqlite"
+	"log/slog"
+	"time"
+)
+
+type App struct {
+	GRPCServer *grpcapp.App
+}
+
+func New(log *slog.Logger, grpcPort int, storagePath string, tokenTTL time.Duration) *App {
+	storage, err := sqlite.New(storagePath)
+	if err != nil {
+		panic(err)
+	}
+
+	authService := auth.New(log, storage, storage, storage, tokenTTL)
+
+	grpcApp := grpcapp.New(log, authService, grpcPort)
+
+	return &App{
+		GRPCServer: grpcApp,
+	}
+}
